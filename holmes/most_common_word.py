fdc9771bb8b98645f876d98aa7cada0953f7e06e
Joseph Clark <joseph.w.clark@asu.edu>
2016-03-19 21:29:21 -0700

    added holmes example

---
+++ holmes/most_common_word.py
@@ -0,0 +1,34 @@
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+
+
+
+class MRMostUsedWord(MRJob):
+
+    def steps(self):
+        return [
+            MRStep(mapper=self.mapper_get_words,
+                   reducer=self.reducer_count_words),
+            MRStep(reducer=self.reducer_find_max_word)
+        ]
+
+    def mapper_get_words(self, _, line):
+        # yield each word in the line
+        words = line.split()
+        for word in words:
+            yield (word.lower(), 1)
+
+    def reducer_count_words(self, word, counts):
+        # send all (num_occurrences, word) pairs to the same reducer.
+        # num_occurrences is so we can easily use Python's max() function.
+        yield None, (sum(counts), word)
+
+    # discard the key; it is just None
+    def reducer_find_max_word(self, _, word_count_pairs):
+        # each item of word_count_pairs is (count, word),
+        # so yielding one results in key=counts, value=word
+        yield max(word_count_pairs)
+
+
+if __name__ == '__main__':
+    MRMostUsedWord.run()
